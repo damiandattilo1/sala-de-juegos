afc4fe5847bd6da44e2209f378694d7933296e97
Damian Dattilo <damiandattilo@gmail.com>
2026-05-20 20:34:40 -0400

    fix: remove authGuard from HomeComponent route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,9 @@ import { QuienSoyComponent } from './quien-soy/quien-soy.component';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
 import { RegistroComponent } from './registro/registro.component';
-import { authGuard } from './auth.guard';
 
 export const routes: Routes = [
-    { path: '', component: HomeComponent, canActivate: [authGuard] },
+    { path: '', component: HomeComponent },
     { path: 'ingresar', component: LoginComponent },
     { path: 'registro', component: RegistroComponent },
     { path: 'quien-soy', component: QuienSoyComponent },
